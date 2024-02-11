02dfcc79b4bd51ccf81eea606935f0a7f0ff6e44
ysunil3 <ysunil3@Ubuntu22.04.03.myguest.virtualbox.org>
2024-02-11 18:03:55 -0500

    Added launch file

---
+++ launch/object_follower_launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='bowser_jr_object_follower',
+            executable='find_object_node',
+            name='find_object',
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(PathJoinSubstitution([
+                FindPackageShare('turtlebot3_bringup'),  # Dynamically finds the package path
+                'launch', 
+                'camera_robot.launch.py'  # Path relative to the package's share directory
+            ]))
+        )
+    ])
