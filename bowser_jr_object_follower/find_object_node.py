a8f3538e084ebd19960abe549da6f775b382c6d8
ysunil3 <ysunil3@Ubuntu22.04.03.myguest.virtualbox.org>
2024-02-11 18:22:36 -0500

    Removed window

--- bowser_jr_object_follower/find_object_node.py
+++ bowser_jr_object_follower/find_object_node.py
@@ -14,7 +14,7 @@ class find_object(Node):
 	def __init__(self):
 		super().__init__('find_object')
 		# Set Parameters
-		self.declare_parameter('show_image_bool', True)
+		self.declare_parameter('show_image_bool', False)
 		self.declare_parameter('window_name', "Raw Image")
 
 		#Determine Window Showing Based on Input
