8f672be6be1461e894d0824860e1116e85def386
ysunil3 <ysunil3@Ubuntu22.04.03.myguest.virtualbox.org>
2024-03-25 14:49:36 -0400

    launchV3

--- launch/go_to_goal_launch.py
+++ launch/go_to_goal_launch.py
@@ -7,16 +7,16 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     return LaunchDescription([
-        # Node(
-        #     package='bowser_jr_object_follower',
-        #     executable='get_nearest_object_node',
-        #     name='get_nearest_object',
-        # ),
         Node(
             package='bowser_jr_object_follower',
-            executable='go_to_goal_node',
-            name='go_to_goal',
+            executable='get_nearest_object_node',
+            name='get_nearest_object',
         ),
+        # Node(
+        #     package='bowser_jr_object_follower',
+        #     executable='go_to_goal_node',
+        #     name='go_to_goal',
+        # ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(PathJoinSubstitution([
                 FindPackageShare('turtlebot3_bringup'),  # Dynamically finds the package path
